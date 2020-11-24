76954bff5206657a49874aacd94f8e9ed138d894
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-11-25 02:08:34 +0300

    ттт

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -63,7 +63,7 @@ for(let i = 0; i < form.length; i++) {
   document.addEventListener('input', (event) => {
     let target = event.target;
     if(target.matches('.form-email') || target.matches('.top-form form-email')){
-        target.value = target.value.replace(/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{3,4})/gi, '');
+        target.value = target.value.replace(/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})/gi, '');
     }
 });
 
@@ -81,9 +81,10 @@ for(let i = 0; i < form.length; i++) {
         
         let target = event.target;
         if(target.matches('.form-phone')){
-            target.value = target.value.replace(/[^+0-9]{5,18}$/);
-            if (target.value.length > 16 && target.value.length !=''  ){
-            alert('введите правильный номер') ;
+            target.value = target.value.replace(/[^+0-9]/gi, '');
+            if (target.value.length > 14 && target.value.length !=''  ){
+             return alert('введите правильный номер', target.value = '');
+            
           }   
         }  
       
